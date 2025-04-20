a7a5326266e6307073947cc9e3b21e95a1d2b626
1appiah <92669349+1appiah@users.noreply.github.com>
2025-04-20 01:10:26 +0000

    vercel added to cors for Yaw

--- Blog/settings.py
+++ Blog/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000','https://anonimage-frontend.onrender.com','https://anonymous-image-react.onrender.com','http://localhost:5173','http://localhost:5173']
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000','https://anonimage-frontend.onrender.com','https://anon-image-frontend.vercel.app','https://anonymous-image-react.onrender.com','http://localhost:5173','http://localhost:5173']
 
 ROOT_URLCONF = 'Blog.urls'
 
